7c296f920b02fab7a8d521f24ef42df4be67e123
shuhulkaul <shuhulkaul22@gmail.com>
2019-10-02 14:49:05 +0530

    make it better

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,10 @@ var express = require('express');
 var router = express.Router();
 const nodemailer = require("nodemailer");
 var flash = require('connect-flash');
-
+var fs = require('fs');
+var path = require('path')
+var Canvas = require('canvas')
+const PDFDocument = require('pdfkit');
 // Get Homepage
 router.get('/', function(req, res)
 {
@@ -16,6 +19,73 @@ router.get('/contact', function(req, res)
 {
 	res.render('contact', {title: 'Contact'});
 });
+
+router.post('/downloadImage', async function(req, res)
+{
+  var text = req.body.text;
+  var textColor = req.body.textColor;
+  var bgColor = req.body.bgColor;
+  var size = req.body.size;
+  if(text)
+  {  
+    Canvas.registerFont('./Sharada.ttf', {family: 'Sharada'})
+    var writeStream = fs.createWriteStream('./image.jpg');
+    var canvas = Canvas.createCanvas(1000, 1000)
+    var ctx = canvas.getContext('2d')
+  
+    var Image = Canvas.Image;
+    var img = new Image();
+    img.src = './layout-'+bgColor+'.jpg';
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(img, 0, 0, canvas.width, canvas.height);
+
+    ctx.fillStyle = textColor;
+    ctx.textAlign = 'center';
+    
+    ctx.font = size+' Sharada'
+    ctx.fillText(text, 500, 500)
+    canvas.createJPEGStream().pipe(writeStream);
+    writeStream.on('finish', function () {
+      res.download('./image.jpg', 'image.jpg');
+    });
+  }
+  else
+  {
+    res.render('home', {msg: 'Please type some text!'});
+  }
+  
+
+});
+router.post('/downloadPDF', async function(req, res)
+{
+  
+  var text = req.body.text;
+  if(text)
+  { 
+      // Create a document
+      const doc = new PDFDocument();
+      
+      // Pipe its output somewhere, like to a file or HTTP response
+      // See below for browser usage
+     var stream = doc.pipe(fs.createWriteStream('document.pdf'));
+      
+      // Embed a font, set the font size, and render some text
+      doc.font('./Sharada.ttf')
+        .fontSize(20)
+        .text(text, 20, 20);
+        doc.end();
+        stream.on('finish', function() {
+          res.download('./document.pdf', 'document.pdf');
+        });
+  }
+
+  else
+  {
+    res.render('home', {msg: 'Please type some text!'});
+  }
+});
+
 
 router.post('/report', async function(req, res)
 {
